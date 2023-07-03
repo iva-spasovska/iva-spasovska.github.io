1d4949cd702a5be842c69552096336b59a06ab28
iva-spasovska <138498882+iva-spasovska@users.noreply.github.com>
2023-07-03 15:38:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.auto-input', {
+    strings: ['student at FINKI.', "Front-End Developer."],
+    typeSpeed: 70,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true,
+});
